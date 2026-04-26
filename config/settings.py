8ec9b169ba6506ea7bb0d94d29ba190d5d69a099
Ente Photo <entephoto.live@gmail.com>
2026-04-27 00:57:06 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -66,13 +66,16 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Explicitly list allowed WebSocket origins for the Channels AllowedHostsOriginValidator.
 # In production replace with your real domain.
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+# ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://entephoto.co.in",
+    "https://www.entephoto.co.in"
 ]
-
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
