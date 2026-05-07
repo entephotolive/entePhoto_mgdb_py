7e748e10aa676a7cd3a340a46563c52cc26b5cdb
Ente Photo <entephoto.live@gmail.com>
2026-05-08 01:09:02 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,21 @@
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.urls import include, path
+
+
+# urlpatterns = [
+#     path("api/", include("recognition.urls")),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
+    path("admin-django/", admin.site.urls),   # Django Admin Panel
     path("api/", include("recognition.urls")),
 ]
 
